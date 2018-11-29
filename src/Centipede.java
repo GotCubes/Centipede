1a38b387fd15d1779c0d10b4d6e33c9ced77f32b
Spencer Deak <spencerdeak@gmail.com>
2018-11-29 12:13:30 -0500

    Centipede restructuring.

--- src/Centipede.java
+++ src/Centipede.java
@@ -1,78 +1,39 @@
-public class Centipede extends Drawable{
+public class Centipede extends Drawable {
 
     boolean dir; // Direction that the centipede is moving.
-    public Centipede next, prev; // The next segment after the current.
     boolean head; // Indicates segment start.
 
-    public Centipede(int x, int y, Centipede n, boolean h) {
-        dir = true;
-        speed = 4;
+    public Centipede(int x, int y, boolean h) {
+        dir = false;
+        speed = 6;
         durability = 2;
         row = x;
         col = y;
-        next = n;
         head = h;
     }
 
     @Override
-    public String toString() { return head ? "\u263A" : "\u25CF"; }
-
-    public void move() {
-        // Default next location.
-        int nxtRow = row;
-        int nxtCol = dir ? col + 1 : col - 1;
-
-        // Get next location if head.
-        if(head) {
-            // Test for collision.
-            if(nxtCol < 0 || nxtCol > 29 || Window.board[row][nxtCol] instanceof Mushroom) {
-                if(row == 27) {
-                    Window.toReverse.add(this);
-                    return;
-                } else {
-                    nxtRow = row + 1;
-                    nxtCol = col;
-                    dir = !dir;
-                }
-            }
-        // Otherwise follow parent.
-        } else {
-            dir = prev.dir;
-            nxtRow = prev.row;
-            nxtCol = prev.col;
-        }
-
-        // Recursively move all segments in the centipede.
-        if(next != null)
-            next.move();
-
-        // Update segment location.
-        row = nxtRow;
-        col = nxtCol;
+    public String toString() {
+        return head ? "\u263A" : "\u25CF";
     }
 
-    public void reverse() {
-        if(this.next == null && this.prev == null) {
-            this.head = true;
-            dir = !dir;
+    public void move(Centipede parent) {
+        // Get next location if head.
+        if (head) {
+            // Test for collision.
+            int nxtCol = col + (dir ? 1 : -1);
+
+            if (nxtCol < 0 || nxtCol > 29 || Window.board[row][nxtCol] instanceof Mushroom) {
+                if (row != 27)
+                    row = row + 1;
+                dir = !dir;
+            } else
+                col = nxtCol;
+            // Otherwise follow parent.
         } else {
-            Centipede curr = this;
-            Centipede temp;
-            curr.head = false;
-
-            while (curr.next != null) {
-                curr.dir = !curr.dir;
-                temp = curr.next;
-                curr.next = curr.prev;
-                curr.prev = temp;
-                curr = curr.prev;
-            }
-
-            curr.head = true;
-            curr.dir = !curr.dir;
-            temp = curr.next;
-            curr.next = curr.prev;
-            curr.prev = temp;
+            dir = parent.dir;
+            row = parent.row;
+            col = parent.col;
         }
     }
 }
